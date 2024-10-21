e18b0a2c17c95ecd3d5d687b1a7b21346d1c6979
SMGX <R.thieu@share.epsb.ca>
2024-10-21 14:32:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// Complete the Phrase
+document.getElementById("story-btn").addEventListener("click", story);
+
+function story() {
+  var output = "story short";
+  alert(output);
+}
+
+document.getElementById("easy-btn").addEventListener("click", easy);
+
+function easy() {
+  var output = "Does it";
+  alert(output);
+}
+
+document.getElementById("devil-btn").addEventListener("click", devil);
+
+function devil() {
+  var output = "devil";
+  alert(output);
+}
+
+document.getElementById("pain-btn").addEventListener("click", pain);
+
+function pain() {
+  var output = "no gain";
+  alert(output);
+}
+
+document.getElementById("time-btn").addEventListener("click", time);
+
+function time() {
+  var output = "When you are having fun";
+  alert(output);
+}
